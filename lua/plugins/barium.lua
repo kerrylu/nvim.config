fbb126562e462ebf11d14a7699104681ae2edec9
Kerry Lu <kerryleonlu@yahoo.com>
2024-03-22 12:24:07 -0700

    Put barium lsp setup in lsp-zero.lua

--- lua/plugins/barium.lua
+++ lua/plugins/barium.lua
@@ -2,9 +2,6 @@ return {
   url = "lukerry@git.amazon.com:pkg/NinjaHooks",
   branch = "mainline",
   lazy = false,
-  dependencies = {
-    {'neovim/nvim-lspconfig'},
-  },
   config = function(plugin)
     vim.opt.rtp:prepend(plugin.dir .. "/configuration/vim/amazon/brazil-config")
     -- Make my own filetype thing to override neovim applying ".conf" file type.
@@ -17,25 +14,6 @@ return {
         end,
       },
     })
-
-    local lspconfig = require 'lspconfig'
-    local configs = require 'lspconfig.configs'
-
-    -- Check if the config is already defined (useful when reloading this file)
-    if not configs.barium then
-        configs.barium = {
-            default_config = {
-                cmd = {'barium'};
-                filetypes = {'brazil-config'};
-                root_dir = function(fname)
-                    return lspconfig.util.find_git_ancestor(fname)
-                end;
-                settings = {};
-            };
-        }
-    end
-
-    lspconfig.barium.setup {}
   end
 }
 
--- lua/plugins/lsp-zero.lua
+++ lua/plugins/lsp-zero.lua
@@ -74,5 +74,23 @@ return {
         return lsp_zero.dir.find_first({'Config'})
       end
     })
+
+    local lspconfig = require 'lspconfig'
+    local configs = require 'lspconfig.configs'
+
+    -- Check if the config is already defined (useful when reloading this file)
+    if not configs.barium then
+        configs.barium = {
+            default_config = {
+                cmd = {'barium'};
+                filetypes = {'brazil-config'};
+                root_dir = function(fname)
+                    return lspconfig.util.find_git_ancestor(fname)
+                end;
+                settings = {};
+            };
+        }
+    end
+    lspconfig.barium.setup {}
   end
 }
