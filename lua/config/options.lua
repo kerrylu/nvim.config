5cc6950ef93d94f5111bdb09bc94ec2551907c93
Kerry Lu <kerryleonlu@yahoo.com>
2024-03-27 20:18:31 -0700

    Remove line numbers

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,16 +2,11 @@
 vim.opt.background = "dark"
 vim.cmd([[colorscheme tokyonight-moon]])
 
--- Display relative and absolute line number
-vim.opt.nu = true
-vim.opt.relativenumber = true
-
 -- Indentation
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-
 vim.opt.smartindent = true
 
 -- Search
